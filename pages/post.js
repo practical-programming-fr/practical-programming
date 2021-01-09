11eebc878730df8272bddf3c3b586fa2e1bb16be
Rayed <rayed.benbrahim@gmail.com>
2021-01-09 16:43:57 +0100

    feat: add prismjs

--- pages/post.js
+++ pages/post.js
@@ -11,16 +11,15 @@ export default function PostPage() {
         }
     }, []);
   return (
-    <div className="flex flex-col">
+      <div>
       <Nav />
-      <section className="flex-grow">
+      <section>
       <div className="flex bg-no-repeat bg-center bg-fixed bg-cover bg-gray-200" style={{backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('../hero.jpg')`, height:'40vh' }}>
         <div className="flex max-w-6xl items-center mx-auto">
             <h1 className="text-center text-4xl md:text-6xl font-medium title-font text-white px-10 lg:px-18">Title of awesome blog post</h1>
         </div>
       </div>
-      <div className="container px-5 py-12 mx-auto -mt-28">
-        <div className="flex flex-wrap -m-4 max-w-6xl">
+      <div className="flex flex-wrap px-5 py-12 max-w-7xl mx-auto -mt-28">
             <div className="p-4 md:w-2/3">
                 <main className="px-8 py-8 bg-white dark:bg-gray-800 rounded-lg shadow-md">
                     <p className="tracking-widest text-md title-font font-medium text-indigo-500 mb-3">Catégorie</p>
@@ -38,20 +37,16 @@ export default function PostPage() {
                     <p className="mt-5">Les données personnelles collectées et traitées sur le Site sont protégées par des mesures techniques et organisationnelles appropriées afin de garantir un niveau de sécurité adapté au risque.</p>
                     <p className="mt-5">L’Utilisateur du Site est informé qu’il dispose d’un droit d’accès, de rectification, de suppression, de limitation, de portabilité (lorsqu’il s’applique) et d’opposition sur les données le concernant. L’Utilisateur peut exercer ces droits en s’adressant au service DPO de RBBM.Tech à l’adresse rgpd@rbbm.tech .</p>
                     <p className="mt-5">L’Utilisateur dispose également du droit d’introduire une réclamation auprès de la CNIL.</p>
-                    <pre className='language-javascript'>
+
+                    <pre className='language-javascript important-overflow-hidden text-sm max-h-60 rounded-lg shadow-lg'>
                         <code>
-{`(async () => {
-    try {
-        await upsertSchemasOnMdm(schemasDirectory);
-        process.exit(0);
-    } catch (exception) {
-        LoggerFactory.getLogger().error(\`putSchemasOnMdmCli - \${exception}\`);
-        process.exit(1);
-    }
-})();`}
+{`<!-- Hello World -->
+function() {
+    console.log('hello world')
+}
+`}
                         </code>
                     </pre>
-
                     <p className="mt-5">Par ailleurs, chaque newsletter envoyée par RBBM.Tech vous indique comment ne plus la recevoir.</p>
                     <p className="mt-5">Les Utilisateurs du Site sont tenus de respecter les dispositions de la loi Informatique et Libertés et du RGPD, notamment en s’abstenant de toute collecte ou utilisation détournée des données à caractère personnel auxquelles ils accèdent.</p>
                     <p className="mt-5">Les Utilisateurs du Site sont tenus de respecter les dispositions de la loi Informatique et Libertés et du RGPD, notamment en s’abstenant de toute collecte ou utilisation détournée des données à caractère personnel auxquelles ils accèdent.</p>
@@ -107,13 +102,11 @@ export default function PostPage() {
 
             </aside>
             </div>
-        </div>
     </div>
                 
       </section>
-      
       <Footer/>
-    </div>
+      </div>
   )
 }
 
