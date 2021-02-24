102432a0883954df02da7be4239c4544c547af99
Rayed <rayed.benbrahim@gmail.com>
2021-02-24 13:50:26 +0400

    V2.0

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -4,7 +4,9 @@ export default sanityClient({
   // Find your project ID and dataset in `sanity.json` in your studio project
   projectId: 'kjg6yd05',
   dataset: 'production',
-  token: '',
+  token:
+    'sk7egF1zKn07Aoa9KKpbAIeZ04eFHTz6DpWOgoVGWR1Cv5B7qeiLIY2qZg8mG9AHbmUq7xob4rSFBQBRpHbjEAsOGjdjbfRuy0uvWwjwIQX33PNGv7BAjtmQoehT4M7rhsqGYfWXKl0AATRJ7ehzBOloQxR07zDb33nSIxmAuXZXGdmxZJY4',
+  useCdn: false,
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
   // Set this to false if your application require the freshest possible
   // data always (potentially slightly slower and a bit more expensive).
--- next.config.js
+++ next.config.js
@@ -1,5 +1,36 @@
 module.exports = {
     images: {
-      domains: ['practicalprogramming.fr', 'cdn.sanity.io'],
+      domains: ['practicalprogramming.fr', 'cdn.sanity.io', 'images.unsplash.com'],
     },
+    async redirects() {
+      return [
+        {
+          source: '/rayed-benbrahim',
+          destination: '/auteur/rayed-benbrahim',
+          permanent: true,
+        },
+        {
+          source: '/lucas-roquilly',
+          destination: '/auteur/lucas-roquilly',
+          permanent: true,
+        },
+        {
+          source: '/13-outils-pour-devenir-un-meilleur-developpeur-node-js',
+          destination: '/12-points-pour-devenir-un-meilleur-developpeur-node-js',
+          permanent: true,
+        },
+        {
+          source: '/boite-a-outils-node-js/how-to-use-node-fs',
+          destination: '/how-to-use-node-fs',
+          permanent: true,
+        }
+      ]
+    },
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        require('./generateSitemap');
+      }
+  
+      return config;
+    }
   }
