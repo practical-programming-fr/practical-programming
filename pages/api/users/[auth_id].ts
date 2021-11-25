4a2d68816dced680422195d218776d8f3d187ec2
Rayed <rayed.benbrahim@gmail.com>
2021-11-25 17:00:17 +0100

    feat: auth user (wip)

---
+++ pages/api/users/[auth_id].ts
@@ -0,0 +1,37 @@
+import connectToDatabase from '../../../lib/mongodb'
+
+const UserService = async (req,res) => {
+  console.log(req.params)
+  let user
+  if (req.method === 'GET' && req.query.auth_id) {
+    const {auth_id} = req.query
+    user = await getUser(auth_id)
+    console.log(user)
+  }
+  if (req.method === 'PATCH' && req.query.auth_id) {
+    const {auth_id} = req.query
+    const userData = req.body
+    user = await updateUser({auth_id},userData)
+    console.log(user)
+  }
+  res.json(user)
+  
+}
+
+const getUser = async (auth_id) => {
+  const client = await connectToDatabase()
+  const db = client.db()
+  const developers = db.collection("developers");
+  return developers.find({auth_id}).toArray()
+}
+
+const updateUser = async (auth_id, userData) =>  {
+  const client = await connectToDatabase()
+  const db = client.db()
+  const developers = db.collection("developers");
+  const updatedUser = await developers.findOneAndUpdate(auth_id,{$set:userData})
+  return updatedUser
+}
+
+export default UserService
+
---
+++ pages/api/users/index.ts
@@ -0,0 +1,29 @@
+import connectToDatabase from '../../../lib/mongodb'
+
+const UsersService = async (req,res) => {
+  console.log(req)
+  let users
+  console.log(req.method)
+  if (req.method === 'GET') {
+    console.log(req.params)
+    users = await getUsers()
+    console.log(users)
+  }
+  if (req.method === 'PATCH') {
+    console.log(req.params)
+    users = await getUsers()
+    console.log(users)
+  }
+  res.json(users)
+  
+}
+
+const getUsers = async () => {
+  const client = await connectToDatabase()
+  const db = client.db()
+  const developers = db.collection("developers");
+  return developers.find().toArray()
+}
+
+export default UsersService
+
